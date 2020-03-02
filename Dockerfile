075dd6e359b914b93359210bb5a4cf19245a8134
yamao <yamao@alala.co.jp>
2020-03-02 14:48:51 +0900

    first commit

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,14 @@
 FROM golang:latest
 
-# Copy apt.conf for proxy server.
-# Remove it if you don't need this.
-COPY ./apt.conf /etc/apt/apt.conf
-
 RUN apt update -y
 RUN apt install -y build-essential sudo
 
-# The following code is impossible under the proxy environment.
-# So you need to execute manually, and then commit its as a new container image.
-# Of course you can uncomment if you are not under the proxy environment.
-# RUN go get -u -d gocv.io/x/gocv
-# RUN cd $GOPATH/src/gocv.io/x/gocv
-# RUN make install
+RUN go get -u -d gocv.io/x/gocv
+RUN cd $GOPATH/src/gocv.io/x/gocv
+RUN make install
 
 WORKDIR /go/src/app
 COPY . .
 
-# RUN go get -d -v ./...
-# RUN go install -v ./...
-# CMD ["app"]
 CMD ["sh"]
 
--- main.go
+++ main.go
@@ -1,9 +1,54 @@
 package main
-
-import(
-  "fmt"
+import (
+    "encoding/json"
+    "fmt"
+    "image"
+    "image/color"
+    "gocv.io/x/gocv"
 )
 
 func main() {
-  fmt.Println("Hello")
+    // Detected by passing learned data to classifier
+    // The data used this time is the data originally included in opencv / data
+    harrcascade := "haarcascade_frontalface_default.xml"
+    classifier := gocv.NewCascadeClassifier()
+    classifier.Load(harrcascade)
+    defer classifier.Close()
+
+    // color for the rect when faces detected
+    blue := color.RGBA{0, 0, 255, 0}
+
+    // read image
+    img := gocv.IMRead("images/person_01.jpg", 0)
+
+    //resize image
+    fact := float64(400) / float64(img.Cols())
+    newY := float64(img.Rows()) * fact
+    gocv.Resize(img, &img, image.Point{X: 400, Y: int(newY)}, 0, 0, 1)
+
+    // detect people in image
+    rects := classifier.DetectMultiScale(img)
+
+    // print found points
+    printStruct(rects)
+
+    // draw a rectangle around each face on the original image,
+    // along with text identifing as "Human"
+    for _, r := range rects {
+        gocv.Rectangle(&img, r, blue, 2)
+        size := gocv.GetTextSize("Human", gocv.FontHersheyPlain, 1.2, 2)
+        pt := image.Pt(r.Min.X+(r.Min.X/2)-(size.X/2), r.Min.Y-2)
+        gocv.PutText(&img, "Human", pt, gocv.FontHersheyPlain, 1, blue, 2)
+    }
+    if ok := gocv.IMWrite("gocv_detected.jpg", img); !ok {
+        fmt.Println("Error")
+    }
+}
+func printStruct(i interface{}) {
+  b, err := json.Marshal(i)
+  if err != nil {
+      fmt.Println(err)
+      return
+  }
+  fmt.Println(string(b))
 }
